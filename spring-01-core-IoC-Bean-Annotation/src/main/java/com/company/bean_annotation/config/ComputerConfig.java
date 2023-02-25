1a49258cc1cb4fe64e851308e09a544ad2ad32a7
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-02-25 15:12:39 -0500

    Configuration Class done

---
+++ spring-01-core-IoC-Bean-Annotation/src/main/java/com/company/bean_annotation/config/ComputerConfig.java
@@ -0,0 +1,44 @@
+package com.company.bean_annotation.config;
+
+import com.company.bean_annotation.caseFactory.Case;
+import com.company.bean_annotation.caseFactory.DellCase;
+import com.company.bean_annotation.monitorFactory.AcerMonitor;
+import com.company.bean_annotation.monitorFactory.Monitor;
+import com.company.bean_annotation.monitorFactory.SonyMonitor;
+import com.company.bean_annotation.motherboardFactory.AsusMotherboard;
+import com.company.bean_annotation.motherboardFactory.Motherboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+
+@Configuration
+public class ComputerConfig {
+
+    @Bean
+    public Case dellCase(){
+        return new DellCase("120A","Dell","220");
+    }
+
+    @Bean(name = "acer")
+    //@Primary
+    public Monitor acerMonitor(){
+        return new AcerMonitor("27inch Beast","Acer",27);
+    }
+
+    @Bean
+    //@Bean(name="sony)
+    public Monitor sonyMonitor(){
+        return new SonyMonitor("34inch C+","Sony",34);
+    }
+
+    @Bean
+    public Motherboard asusMotherboard(){
+        return new AsusMotherboard("v5.5","Asus",4,3,"v5");
+    }
+
+
+
+
+
+}

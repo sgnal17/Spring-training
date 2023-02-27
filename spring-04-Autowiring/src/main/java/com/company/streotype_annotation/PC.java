d6e79ee1f73123ef8135e3d5f5e08a9fb682a0a7
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-02-26 23:23:39 -0500

    Auto Wiring

---
+++ spring-04-Autowiring/src/main/java/com/company/streotype_annotation/PC.java
@@ -0,0 +1,34 @@
+package com.company.streotype_annotation;
+
+import com.company.streotype_annotation.casefactory.Case;
+import com.company.streotype_annotation.monitorfactory.Monitor;
+import com.company.streotype_annotation.motherboardfactory.Motherboard;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class PC {
+
+    private Case theCase;
+    private Monitor monitor;
+    private Motherboard motherboard;
+
+
+    public PC(Case theCase, Monitor monitor, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitor = monitor;
+        this.motherboard = motherboard;
+    }
+
+    public void powerUp() {
+        theCase.pressPowerButton();
+        drawLogo();
+    }
+
+    private void drawLogo() {
+
+        monitor.drawPixelAt();
+    }
+
+}

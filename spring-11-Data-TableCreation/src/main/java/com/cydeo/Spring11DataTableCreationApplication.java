95283493a172cc814c6e238e2669e7314ef0bb99
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-04-10 15:25:01 -0400

    connection done

---
+++ spring-11-Data-TableCreation/src/main/java/com/cydeo/Spring11DataTableCreationApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring11DataTableCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring11DataTableCreationApplication.class, args);
+	}
+
+}
---
+++ spring-11-Data-TableCreation/src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+drop table if exists employees;
+
+create table employees(
+    id bigint not null ,
+    name varchar(255),
+    primary key (id)
+);

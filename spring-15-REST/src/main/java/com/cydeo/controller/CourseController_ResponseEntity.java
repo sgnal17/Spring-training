32307ec3ed58aa02157168ed8f39e9fcb09e444b
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-05-27 11:01:55 -0400

    Api creation with ResponseEntity

---
+++ spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
@@ -0,0 +1,53 @@
+package com.cydeo.controller;
+
+
+import com.cydeo.dto.CourseDTO;
+import com.cydeo.service.CourseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses/api/v2 ")
+public class CourseController_ResponseEntity {
+
+    private final CourseService courseService;
+
+
+    public CourseController_ResponseEntity(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CourseDTO>> getAllCourses(){
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED) //manipulating http status code 202
+                .header("Version","Cydeo.V2")
+                .header("Operation","Get")
+                .body(courseService.getCourses());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(courseService.getCourseById(id)); // if we don't want to add header or anything
+    }
+
+    @GetMapping("/category/{categoryName}")
+    public ResponseEntity<List<CourseDTO>> getCourseByCategory(@PathVariable("categoryName") String name){
+        return ResponseEntity.ok(courseService.getCoursesByCategory(name));
+    }
+
+    @PostMapping
+    public ResponseEntity<CourseDTO> createCourse(@RequestBody CourseDTO dto){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .header("Operation","CREATE")
+                .body(courseService.createCourse(dto));
+    }
+
+
+
+
+}

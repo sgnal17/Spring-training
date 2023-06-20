5341323a807a305dc59436fade48aa5cceb20119
sgnal17 <96966147+sgnal17@users.noreply.github.com>
2023-06-20 00:10:45 -0400

    practice

---
+++ spring-18-REST-Security-Keycloak-LicenseApp/src/main/java/com/cydeo/Spring18RestSecurityKeycloakLicenseAppApplication.java
@@ -0,0 +1,18 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Spring18RestSecurityKeycloakLicenseAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring18RestSecurityKeycloakLicenseAppApplication.class, args);
+    }
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
